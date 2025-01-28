1a81f27a31bc15876570e58929e9e77206dff3c5
EwanBailey <ewan.bailey@gmail.com>
2025-01-28 09:02:40 -0800

    Add smoothing variable

--- Accel.py
+++ Accel.py
@@ -1,12 +1,13 @@
 import time
 from adafruit_circuitplayground import cp
 
+data_smoothing = 10
 
 while True:
     x, y, z = cp.acceleration
-    x, y, z = x*10, y*10, z*10
+    x, y, z = x*data_smoothing, y*data_smoothing, z*data_smoothing
     x, y, z = int(x), int(y), int(z)
-    X, Y, Z = x/10, y/10, z/10
+    X, Y, Z = x/data_smoothing, y/data_smoothing, z/data_smoothing
     print((X, Y, Z))
 
     time.sleep(0.01)
