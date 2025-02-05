e9f98fef9422e0b4bb104b86ba1ef073ae3f932d
EwanBailey <ewan.bailey@gmail.com>
2025-02-05 09:29:28 -0800

    Update Accel.py

--- Accel.py
+++ Accel.py
@@ -4,15 +4,18 @@ from adafruit_circuitplayground import cp
 
 while True:
     x, y, z = cp.acceleration
-    x, y, z = float(x), float(y), float(z)
+    x, y = float(x), float(y)
+    
     angle = math.atan2(-y, -x)
-    angle = angle*180/(math.pi)
+    angle = 270+angle*180/(math.pi)
+    angle = int((angle)/36) % 10
+    
     magnitude = math.sqrt(x**2+y**2)
-    print(angle, magnitude)
-    angle = int((270+angle)/36)
-    angle = angle % 10
+        
     cp.pixels[angle] = (5*magnitude, 0, 0)
+    
     for i in range(10):
         if(i != angle):
-            cp.pixels[i] = (0, 0, 0)            
+            cp.pixels[i] = (0, 0, 0)
+            
     time.sleep(0.01)
